c79dc549c29e33b46d1836ea0ef808a054ab8c62
Josh Brackett <joshua.brackett@hotmail.ca>
2018-05-24 22:07:02 -0400

    pickups float up into the sky now video 3 done

--- UT/Source/UT/Pickup.cpp
+++ UT/Source/UT/Pickup.cpp
@@ -37,6 +37,10 @@ void APickup::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector NewLocation = GetActorLocation();
+	NewLocation.Z += (DeltaTime * 100.0f);
+	SetActorLocation(NewLocation);
+
 }
 
 void APickup::OnPlayerEnterPickupBox(UPrimitiveComponent * OverlappedComp, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult) {
